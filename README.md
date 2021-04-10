697e2f30dd55ebce0b80ace4350b45cf2bf067ad
m4dh0rs3 <schoeps.benedikt@gmail.com>
2021-04-10 15:35:23 +0200

    insert prototype

--- README.md
+++ README.md
@@ -4,6 +4,21 @@
 
 Two players alternate choosing unique tiles of radii 1 to 3 and 4 directions to append to the end of the last tile, shaping a path on the board. If such intersects itself, all points enclosed by its shape are taken by the player who closed the path. He aims to accumulate the most points and not to steer outside the board.
 
+### Technical
+
+#### Steps
+
+1. Offer a set of tiles to the player to choose from.
+2. Append the tile a the end of the last tile.
+    1. Rotate the tile around the arrow.
+    2. The midpoint of the circular arc is the position of the arrow plus a vector of the radius of the arc in the direction of the start angle of the arc.
+    3. Move the arrow to the end of the tile.
+3. Find all intersections of the last tile with the path.
+    1. Find the intersection point of the two circles and check if they are contained by the arc
+4. For every intersection, find the polygon enclosed by the path.
+    1. The set of curves between the intersection indecies are the polygon.
+5. For every free point on the board, check if the winding number of any polygon is not zero. Then the point can be marked by the player.
+
 ### Contributors
 
 I have written this private clone of [Polycentrics](https://www.polycentrics.com/), a game by `Angelo Alessandro Mazzotti` (registered `Jan 2018`). It was initially implemented by `OrionLab` and published by [GAMMAGRAPHICS SRLS](https://www.gammagraphics.eu/).
--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,7 +1,94 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use wasm_bindgen::prelude::*;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+use yew::prelude::*;
+
+use backend::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    App::<Polycentrics>::new().mount_to_body();
+}
+
+/// Holds game and UI state.
+struct Polycentrics {
+    link: ComponentLink<Self>,
+    board: Board,
+    canvas_ref: NodeRef,
+    canvas_size: u16,
+    canvas_scale: f64,
+    canvas_offset: f64,
+}
+
+/// UI messages.
+enum Msg {
+    DrawBoardCanas,
+    SetTile(usize),
+}
+
+/// Properties a [`Polycentrics`] game can
+/// have from html.
+#[derive(Clone, Properties)]
+struct GameProps {
+    canvas_size: u16,
+}
+
+impl Default for GameProps {
+    fn default() -> Self {
+        Self { canvas_size: 400 }
+    }
+}
+
+/// Implemen [`Polycentrics`] as an html element.
+impl Component for Polycentrics {
+    type Message = Msg;
+    type Properties = GameProps;
+
+    /// Returns new element as html.
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        // first draw call
+        link.send_message(Msg::DrawBoardCanas);
+
+        Self {
+            link,
+            board: Board::default(),
+            canvas_ref: NodeRef::default(),
+            canvas_size: props.canvas_size,
+            canvas_scale: props.canvas_size as f64 / 11.0,
+            canvas_offset: props.canvas_size as f64 / 22.0,
+        }
+    }
+
+    /// Updates the UI based on an action message.
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::DrawBoardCanas => {
+                self.draw();
+
+                true
+            }
+            Msg::SetTile(tile) => {
+                self.board.step(tile);
+
+                self.link.send_message(Msg::DrawBoardCanas);
+                false
+            }
+        }
+    }
+
+    /// Default set to false, as ingame properties should not change.
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    /// Return html.
+    fn view(&self) -> Html {
+        html! {
+            <div class="polycentrics">
+                { self.tiles_nodes_view() }
+                { self.board_canvas_view() }
+                { self.tile_pad_view() }
+            </div>
+        }
     }
 }
