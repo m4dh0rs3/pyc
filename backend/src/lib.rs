804875413a25c3719da8549ef2cf945f3c091868
Benedikt Schöps <schoeps.benedikt@gmail.com>
2021-07-11 21:18:06 +0200

    rewrite part 1, also curve intersection test

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,15 +1,9 @@
 //! # Polycentrics
-//! Game logic of Polycentrics, including the board and virtual players.
+//! Game logic (WASM Server) of Polycentrics, including the board and virtual players.
 
 mod game;
 mod math;
-
+// re-export for frontend use
 pub mod prelude {
-    pub use crate::{
-        game::{
-            board::{Board, Player},
-            curve::Curve,
-        },
-        math::prelude::*,
-    };
+    pub use crate::{ math::prelude::*};
 }
